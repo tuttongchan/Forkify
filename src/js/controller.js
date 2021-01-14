2ee0568ee1d7ca9270ca73040497145cb8208d1a
Tut Tongchantra <tuttongchan@Tuts-MacBook-Air.local>
2021-01-13 21:41:52 -0800

    first commit

---
+++ src/js/controller.js
@@ -0,0 +1,44 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+import icons from 'url:../img/icons.svg'; //Parcel 2
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+
+    // 2) Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes));
+
+// window.addEventListener('hashchange', controlRecipes)
+// window.addEventListener('load', controlRecipes)
